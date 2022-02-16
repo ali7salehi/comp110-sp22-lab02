ae097be1325178f576086a473d54b257c2e7a376
Ali Salehi <asalehi@sandiego.edu>
2022-02-16 12:32:05 -0800

    Finished Ali_drawer

--- inner_pentagon.py
+++ inner_pentagon.py
@@ -22,4 +22,30 @@ sat.left(72)
 sat.forward(50)
 sat.left(72)
 
+# create a turtle and set the pen color "Green"
 
+sat.pencolor("green")
+sat.penup()
+
+#sat.setheading()
+sat.goto(10)
+sat.setheading(15)
+sat.pendown()
+sat.setheading(0)
+
+
+# draw the pentegan.
+
+sat.forward(40)
+sat.left(72)
+sat.forward(40)
+sat.left(72)
+sat.forward(40)
+sat.left(72)
+sat.forward(40)
+sat.left(72)
+sat.forward(40)
+sat.left(72)
+
+turtle_window = turtle.Screen()
+turtle_window.exitonclick()
